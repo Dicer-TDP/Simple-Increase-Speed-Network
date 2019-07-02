ef7d6806e42bb7f4d9da6fce2ff1a1efaca29546
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2019-07-02 21:22:18 +0700

    Add files via upload

---
+++ speedo.bat
@@ -0,0 +1,7 @@
+ipconfig/flushdns
+ipconfig/renew
+netsh branchcache flush
+netsh interface tcp set global autotuning=disabled
+netsh interface tcp set global rss=dsiabled
+netsh interface tcp set global chimney=disabled
+netsh interface tcp set heuristics disabled
