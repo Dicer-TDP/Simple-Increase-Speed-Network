e0f91d8f3685658eb48d943216da8e6ded1d2407
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2019-07-05 17:44:08 +0700

    Delete speedo.bat

--- speedo.bat
+++
@@ -1,7 +0,0 @@
-ipconfig/flushdns
-ipconfig/renew
-netsh branchcache flush
-netsh interface tcp set global autotuning=disabled
-netsh interface tcp set global rss=dsiabled
-netsh interface tcp set global chimney=disabled
-netsh interface tcp set heuristics disabled
